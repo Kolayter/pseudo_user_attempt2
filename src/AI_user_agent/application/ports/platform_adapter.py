669eac78b89af7b01aad8655f59ad5998f871a58
Kolayter <kolayter5441@gmail.com>
2026-08-26 01:17:48 +0300

    Wrote reciver and sender classes

---
+++ src/AI_user_agent/application/ports/platform_adapter.py
@@ -0,0 +1,29 @@
+# application/ports/platform_adapter.py
+from abc import abstractmethod, ABC
+
+class IMessageReciver(ABC):
+
+    @abstractmethod
+    async def send_message(self, chat_id: int, text: str) -> None:
+        ...
+    
+    # @abstractmethod
+    # async def send_reaction(self, chat_id: int, message_id: int) -> None:
+    #     ...
+
+    @abstractmethod
+    async def typing_status(self, chat_id) -> None:
+        ...
+
+
+class IMessageSender(ABC):
+
+    @abstractmethod
+    async def start(self) -> None:
+        ...
+    
+    @abstractmethod
+    async def stop(self) -> None:
+        ...
+
+    
